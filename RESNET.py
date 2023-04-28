9ba540a0a03adf98c59cd2594ccbacdfa5abccf1
sabelo <dlmsab007@myuct.ac.za>
2023-04-28 16:37:15 +0200

    version 2.0 RESNET : Implemented the Residual block model and defined the RESNET architecture with 2 convolutional layers. First epoch took more than 30 minutes

--- RESNET.py
+++ RESNET.py
@@ -19,22 +19,78 @@ def dataset():
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
 
-    batch_size = 512
+    batch_size = 128
     global train_loader,test_loader
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,shuffle=True)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
 
 
+# Define the RESNET architecture
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+    
+class ResidualBlock(nn.Module):
+     def __init__(self, block):
+        super(ResidualBlock, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, 2, stride=1)
+        self.layer2 = self._make_layer(block, 128, 2, stride=2)
+        self.layer3 = self._make_layer(block, 256, 2, stride=2)
+        self.layer4 = self._make_layer(block, 512, 2, stride=2)
+        self.linear = nn.Linear(512* block.expansion, 10)
+
+     def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+     def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
 def model_setup():
     # Identify device
-    global device,cnn, resnet
+    global device, resnet
     device = ("cuda" if torch.cuda.is_available()
         else "mps" if torch.backends.mps.is_available()
         else "cpu"
     )
     print(f"Using {device} device")
 
-    resnet = ResidualBlock().to(device)
+    resnet = ResidualBlock(BasicBlock).to(device)
 
 # Define the training and testing functions
 def train(net, train_loader, criterion, optimizer, device):
@@ -88,7 +144,7 @@ def savep():
 
 def loadp():
     print("Loading params...")
-    model = RESNET()
+    model = ResidualBlock()
     model.load_state_dict(torch.load("RESNETsavedmodel.pt"))
     model.eval()
     print("Done!")
