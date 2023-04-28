20e68d5b32d6245593b11095f77c30dc8b9f6fab
sabelo <dlmsab007@myuct.ac.za>
2023-04-28 14:07:06 +0200

    version 1.0 RESNET : defined train and test functions for the resnet model, which is training train data and train test data that the model has not seen to test model perfomance

--- RESNET.py
+++ RESNET.py
@@ -36,6 +36,36 @@ def model_setup():
 
     resnet = ResidualBlock().to(device)
 
+# Define the training and testing functions
+def train(net, train_loader, criterion, optimizer, device):
+    net.train()  # Set model to training mode.
+    running_loss = 0.0  # To calculate loss across the batches
+    for data in train_loader:
+        inputs, labels = data  # Get input and labels for batch
+        inputs, labels = inputs.to(device), labels.to(device)  # Send to device
+        optimizer.zero_grad()  # Zero out the gradients of the ntwork i.e. reset
+        outputs = net(inputs)  # Get predictions
+        loss = criterion(outputs, labels)  # Calculate loss
+        loss.backward()  # Propagate loss backwards
+        optimizer.step()  # Update weights
+        running_loss += loss.item()  # Update loss
+    return running_loss / len(train_loader)
+
+def test(net, test_loader, device):
+    net.eval()  # We are in evaluation mode
+    correct = 0
+    total = 0
+    with torch.no_grad():  # Don't accumulate gradients
+        for data in test_loader:
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device) # Send to device
+            outputs = net(inputs)  # Get predictions
+            _, predicted = torch.max(outputs.data, 1)  # Get max value
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()  # How many are correct?
+    return correct / total
+
+
 def epoch_resnet():
     LEARNING_RATE = 1e-1
     MOMENTUM = 0.9
